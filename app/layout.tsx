c6bb34d5451b2ff8e49a08926e08c7a3e0f8bcaa
RahulPand152 <rahultoprove982687@gmail.com>
2026-05-22 12:20:36 +0545

    feat: initialize root layout and global Tailwind CSS configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
         suppressHydrationWarning
       >
         <Providers>
